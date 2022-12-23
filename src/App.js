f2afccbeff9cd03c7daa1602be3b8b5c1ab3c2a5
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2022-12-23 10:50:27 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,116 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+
 import logo from '../img/logo.png';
+import m from '../img/m.jpg';
+import wm from '../img/wm.jpg';
 import './Footer.css'
+
+
+
 const Footer = () => {
-    return(
-        
-       <div className='footer' >
-        <div className='container'>
-            <div className='grid'>
-            <div className='gridhead' ><h3>INTEGRANTES</h3> <br></br></div>
-                <div>
-                
-                    <a href='logo.png'><img src={logo} alt='logo' ></img></a>
-                    <p>Las mejores Recetas</p>
+    return (
+
+        <div className='footer' >
+            <div className='container'>
+                <div className='grid'>
+                    <div className='gridhead' > <br></br></div>
+                    <div>
+
+                        <a href='logo.png'><img src={logo} alt='logo' ></img></a>
+                        <p>ComidApp<br/>Las mejores Recetas</p>
+                    </div>
+
+
+                    <div>
+                        <Carousel>
+                            <Carousel.Item>
+                                <img className="rounded-circle" src={m} alt="First slide" />
+                                <br></br><br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+
+                                    <p>Matías Nicolas Bravo Nerón</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="rounded-circle" src={wm} alt="Second slide" />
+                                <br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+
+                                    <p>Florencia Ojeda</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="rounded-circle" src={wm} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Paula de Figueiredo</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Braian D'Aleo</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Daniel Alberto Rocha Bartaburu</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item className="justify-content-sm-center">
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Dario Fabian Guardiola</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item className="justify-content-sm-center">
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Facundo G. Miranda</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item className="justify-content-sm-center">
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br>
+
+                                    <p>Juan David Chaves</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item className="justify-content-sm-center">
+                                <img className="rounded-circle" src={m} alt="Third slide" />
+                                <br></br><br></br><br></br><br></br><br></br>
+                                <Carousel.Caption>
+                                    <br></br><br></br><br></br><br></br>
+
+                                    <p>Ramiro Hittaller</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </div>
                 </div>
-                <div>
-                <br></br>
-                    <ul>
-                        <li>Inicio</li>
-                        <li>Recetas</li>
-                        <li>Faqs</li>
-                        <li>Contact</li>
-                        
-                    </ul>
-                </div>
-                
-                <div>
-                    <ul>
-                        <li>Matías Nicolas Bravo Nerón</li>
-                        <li>Juan David Chaves</li>
-                        <li>Daniel Alberto Rocha Bartaburu</li>
-                        <li>Facundo G. Miranda</li>
-                        <li>Dario Fabian Guardiola</li>
-                    </ul>
-                </div>
-                <div>
-                <br></br>
-                <ul>
-                        <li>Paula de Figueiredo</li>
-                        <li>Florencia Ojeda</li>
-                        <li>Ramiro Hittaller</li>
-                        <li>Braian D'Aleo</li>
-                        
-                    </ul>
-                </div>
+                <p> Creado con React, SweetAlert, Bootstrap, Firebase  · Maquetado by Webflow</p>
             </div>
-            <p> Creado con React, SweetAlert, Bootstrap, Firebase  · Maquetado by Webflow</p>
+
         </div>
 
-       </div>
-        
     )
 }
 export default Footer
